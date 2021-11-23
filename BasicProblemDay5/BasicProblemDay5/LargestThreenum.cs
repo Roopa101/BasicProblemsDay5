e825cba7919d234b086e9406d30df3179f817677
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-23 19:44:45 +0530

    [Roopa] Checking the largest number among three numbers

---
+++ BasicProblemDay5/BasicProblemDay5/LargestThreenum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblemDay5
+{
+    class LargestThreeNumbers
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the Number 1");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Number 2");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Number 3");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine("Number 1 is greater" +num1);
+            }
+            else if (num2 > num3)
+            {
+                Console.WriteLine("Number 2 is greater" +num2);
+            }
+            else
+            {
+                Console.WriteLine("Number 3 is greater" +num3);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
