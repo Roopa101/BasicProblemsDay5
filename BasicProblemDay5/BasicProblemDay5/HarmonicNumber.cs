03fcbd0a4a4169e386f0eeb18a9c62b730ba219a
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-23 16:22:08 +0530

    [Roopa] Printing the Harmonic number

---
+++ BasicProblemDay5/BasicProblemDay5/HarmonicNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblemDay5
+{
+    class HarmonicNumber
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the last harmonic number");
+            double num = Convert.ToDouble(Console.ReadLine());
+            double harmonicSum = 0;
+            for (double i = 1; i <= num; i++)
+            {
+                Console.WriteLine("1/" + i + "=" + (1 / i));
+                harmonicSum += (1 / i);
+            }
+            Console.WriteLine("The Sum of harmonic number is :" + harmonicSum);
+            Console.ReadLine(); 
+        }
+
+    }
+}
