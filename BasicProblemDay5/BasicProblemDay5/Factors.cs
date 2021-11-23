09012c0137452360c0ad6775948d8889bd1ea3ee
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-23 18:17:38 +0530

    [Roopa] Finding the primeFactorization

---
+++ BasicProblemDay5/BasicProblemDay5/Factors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblemDay5
+{
+    class Factors
+    {
+        static void Main(string[] args)
+        {
+            
+            int fact = 1;
+            int i;
+            Console.WriteLine("Enter the input number:");
+            int number = int.Parse(Console.ReadLine());
+            for (i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("factorial of" + number + "is  " + fact );
+            Console.ReadLine();
+        }
+    }
+}
+    
