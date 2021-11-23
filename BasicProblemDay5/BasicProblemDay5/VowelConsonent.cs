aa12f90a7ea4bc9b43aa410af4566c376dec4d94
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-23 19:41:53 +0530

    [Roopa] Checking the letter whether vowel or Consonent

---
+++ BasicProblemDay5/BasicProblemDay5/VowelConsonent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblemDay5
+{
+    class VowelConsonent
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the Letter");
+            char letter = Convert.ToChar(Console.ReadLine());
+
+            if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+            {
+                Console.WriteLine("Letter is a Vowel" +letter);
+            }
+            else
+            {
+                Console.WriteLine("Letter is a Consonent" +letter);
+            }
+            Console.ReadLine();
+        }
+    }
+}
