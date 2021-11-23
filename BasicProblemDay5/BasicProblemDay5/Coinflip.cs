85813032c53b4b69ce049e99899a84d30877f7e4
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-23 14:40:58 +0530

    [Roopa] Flipping coin and Checking Percentage

---
+++ BasicProblemDay5/BasicProblemDay5/Coinflip.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblemDay5
+{
+    class Coinflip
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Enter the number of times to flip coin");
+            double num = Convert.ToDouble(Console.ReadLine());
+            Random random = new Random();
+            double headCount = 0, tailCount = 0;
+            for (int i = 0; i < num; i++)
+            {
+                double flip = random.NextDouble();
+                if (flip < 0.5)
+                {
+                    Console.WriteLine("Tails");
+                    tailCount++;
+                }
+                else
+                {
+                    Console.WriteLine("Heads");
+                    headCount++;
+                }
+            }
+            headCount *= (100 / num);
+            tailCount *= (100 / num);
+            Console.WriteLine("The Percentage of Heads vs Tails is {0}% and {1}%", headCount, tailCount);
+            Console.ReadKey();
+        }
+    }
+}
