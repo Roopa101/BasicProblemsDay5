481c4150fe60f5c2f6a374bf9bc9937603010603
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-23 19:06:18 +0530

    [Roopa] Checking the num whether even or odd

---
+++ BasicProblemDay5/BasicProblemDay5/EvenOdd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblemDay5
+{
+    class EvenOrOdd
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the number to Check");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("{0} is an Even number", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is an Odd number", num);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+ 
