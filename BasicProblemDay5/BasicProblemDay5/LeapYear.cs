69e681cd0ccf6e56c614ddbced5f07bc9cd7cc8a
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-23 14:53:07 +0530

    [Roopa] Finding the year is Leap year or not

---
+++ BasicProblemDay5/BasicProblemDay5/LeapYear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblemDay5
+{
+    class LeapYear
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+            {
+                Console.WriteLine("Year is a Leap Year", +year);
+            }
+            else
+            {
+                Console.WriteLine("Year is not a Leap Year", +year);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+    
