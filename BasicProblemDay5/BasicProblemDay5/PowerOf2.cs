183c011be7a3accf732708bfd8d6c22ade2daac2
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-23 16:10:09 +0530

    [Roopa] printing a table of the power 2

---
+++ BasicProblemDay5/BasicProblemDay5/PowerOf2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblemDay5
+{
+    class PowerOf2
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the no. between 1 to 31");
+            int Number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= Number; i++)
+            {
+                Console.WriteLine("2^" + i + "=" + Math.Pow(2, i));
+            }
+            Console.ReadLine();
+        }
+    }
+}
+    
