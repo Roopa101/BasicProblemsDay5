6d22ae9689b1fe1a6c51290af1bccb5f60aaa428
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-23 18:56:49 +0530

    [Roopa] Finding the Quotient and Remainder

---
+++ BasicProblemDay5/BasicProblemDay5/QuotientRemainder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblemDay5
+{
+    class QuotientRemainder
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the Dividend");
+            double dividend = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the Divisor");
+            double divisor = Convert.ToDouble(Console.ReadLine());
+
+            double quotient = dividend / divisor;
+            double remainder = dividend % divisor;
+
+            Console.WriteLine("Quotient is : " + quotient);
+            Console.WriteLine("Remainder is : " + remainder);
+            Console.ReadLine();
+        }
+
+    }
+}
+
